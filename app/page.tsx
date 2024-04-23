d9b9b5338374ed1e067c78c5f6fcf5d87f7eaeef
Milos Tanaskovic <mitan@Miloss-MacBook-Air.local>
2024-04-24 00:59:25 +0200

    setup auth - signin and signup.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
       <h1 className="h1-bold">Nextjs</h1>
       <h2 className="h2-bold">Nextjs</h2>
       <h3 className="h3-bold">Nextjs</h3>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
