c20ef5f7cf5753f7500dda510421761750e27aeb
Milos Tanaskovic <mitan@Miloss-MacBook-Air.local>
2024-04-29 00:02:32 +0200

    implement theme mode...

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 
 import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface ThemeContextProps {
-  mode: "dark" | "light";
-  setMode: (mode: "dark" | "light") => void;
+  mode: "dark" | "light" | "system";
+  useMode: (mode: "dark" | "light" | "system") => void;
 }
 
 interface ThemeProviderProps {
@@ -16,31 +17,39 @@ export const ThemeContext = createContext<ThemeContextProps | undefined>(
 );
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-  const [mode, setMode] = useState<"dark" | "light">("dark");
+  const [mode, useMode] = useState<"dark" | "light" | "system">("light");
 
-//   const handleThemeChange = () => {
-//     if (mode === "dark") {
-//       setMode("light");
-//       document.documentElement.classList.add("light");
-//     } else {
-//       setMode("dark");
-//       document.documentElement.classList.add("dark");
-//     }
-//   };
+  const handleThemeChange = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      useMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      useMode("light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
 
   useEffect(() => {
-    // Apply the class to the document element when mode changes
-    const className = mode === "dark" ? "dark" : "light";
-    document.documentElement.classList.add(className);
-
-    // Clean up: remove the class when the component unmounts or mode changes again
-    return () => {
-      document.documentElement.classList.remove(className);
-    };
+    handleThemeChange();
   }, [mode]);
 
+  // useEffect(() => {
+  //   // Apply the class to the document element when mode changes
+  //   const className = mode === "dark" ? "dark" : "light";
+  //   document.documentElement.classList.add(className);
+
+  //   // Clean up: remove the class when the component unmounts or mode changes again
+  //   return () => {
+  //     document.documentElement.classList.remove(className);
+  //   };
+  // }, [mode]);
+
   return (
-    <ThemeContext.Provider value={{ mode, setMode }}>
+    <ThemeContext.Provider value={{ mode, useMode }}>
       {children}
     </ThemeContext.Provider>
   );
