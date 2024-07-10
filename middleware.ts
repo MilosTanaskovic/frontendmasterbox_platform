a2428249835fc80ac1d3f2d78e45aae0b5e9b8d8
Milos Tanaskovic <mitan@Miloss-MacBook-Air.local>
2024-07-10 12:51:33 +0200

    add public routes..

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isPublicRoute = createRouteMatcher([
-//   "/",
-//   "/api/webhook",
-//   "/questions/:id",
-//   "/tags",
-//   "/tags/:id",
-//   "/profile/:id",
-//   "/community",
-//   "/jobs",
-// ]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhook",
+  "/questions/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "/community",
+  "/jobs",
+]);
 
-const isProtectedRoute = createRouteMatcher(["/api/webhook", "/api/chatgpt"]);
+// const isProtectedRoute = createRouteMatcher(["/api/webhook", "/api/chatgpt"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
